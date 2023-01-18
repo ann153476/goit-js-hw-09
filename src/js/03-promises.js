d180be555089466664d76d55c8d7bc29e0ff33e3
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-18 18:24:43 +0200

    new

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,27 @@
-import Notiflix from 'notiflix';//Notiflix.Notify.success(
+// import Notiflix from 'notiflix';//Notiflix.Notify.success(
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
   
-}
+// }
 
-const delay = document.querySelector('[name="delay"]')
-const step = document.querySelector('[name="step"]')
-const amount = document.querySelector('[name="amount"]')
-const btn = document.querySelector('button')
-const form = document.querySelector('.form')
+// const delay = document.querySelector('[name="delay"]')
+// const step = document.querySelector('[name="step"]')
+// const amount = document.querySelector('[name="amount"]')
+// const btn = document.querySelector('button')
+// const form = document.querySelector('.form')
 
-let obj={};
-function f(event){
-  event.preventDefault();
-  obj[event.target.name] = event.target.value;
-  console.log(obj,"<");
-  //createPromise();
-}
+// let obj={};
+// function f(event){
+//   event.preventDefault();
+//   obj[event.target.name] = event.target.value;
+//   console.log(obj,"<");
+//   //createPromise();
+// }
 
-form.addEventListener('submit', f)
+// form.addEventListener('submit', f)
