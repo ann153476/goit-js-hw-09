32a7ff1daf7b19e1b0166c3d1be218d45755a839
ann153476 <103377387+ann153476@users.noreply.github.com>
2023-01-19 18:10:29 +0000

    Deploying to gh-pages from @ ann153476/goit-js-hw-09@d0fdcc5ef8fd617b56125178d8bcd7f86efe3e5a 🚀

---
+++ 01-color-switcher.9936e5bf.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",(function(){t.disabled=!0,n.disabled=!1,a=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){n.disabled=!0,t.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.9936e5bf.js.map
